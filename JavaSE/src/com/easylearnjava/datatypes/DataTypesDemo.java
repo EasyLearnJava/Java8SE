8177a497a9c5ab3fbc9ad5d72cec921035cdc42a
Easy Learn Java <easylearnjava4u@gmail.com>
2018-07-22 22:49:42 -0500

    Update DataTypesDemo.java

--- JavaSE/src/com/easylearnjava/datatypes/DataTypesDemo.java
+++ JavaSE/src/com/easylearnjava/datatypes/DataTypesDemo.java
@@ -2,4 +2,34 @@ package com.easylearnjava.datatypes;
 
 public class DataTypesDemo {
 
+  public void add(){
+		//int y =0;
+	}
+	
+	public static void main(String[] args) {
+		
+		//x is a variable of type int that can store numbers
+		int x = 5; //
+		
+		x = 12;
+		
+		int z;
+		z = 9;
+		
+		byte b = 127;
+		
+		boolean y = true;
+		
+		char c = 'a';
+		
+		System.out.println(x);
+		System.out.println("The value of x is : "  +  x);
+		
+		x = 23;
+		
+		System.out.println("The value of x is : "  +  x);
+		//int y = true;
+		
+	}
+
 }
