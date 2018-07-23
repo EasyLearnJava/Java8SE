48480926c5f30d7c72db8c69528dd03c6240b41b
34c07d4b798222f71c09 <34c07d4b798222f71c09@2012R2-us-west>
2018-07-22 20:39:10 -0700

    Adding new files

---
+++ JavaSE/src/com/easylearnjava/basics/Comments.java
@@ -0,0 +1,23 @@
+package com.easylearnjava.basics;
+
+public class Comments {
+
+	public static void main(String[] args) {
+
+		// This is a print statement
+		System.out.println("Hi...");
+
+		// This is a single line comment
+
+		/*
+		 * This is a 
+		 * multiline comment
+		 * 
+		 */
+
+		// The below code will not be executed
+		// System.out.println("Test output");
+
+	}
+
+}
